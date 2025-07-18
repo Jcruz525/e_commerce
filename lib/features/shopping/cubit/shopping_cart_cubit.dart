72ca0c3a252d968667c76e4c8b84ec09b689a800
Jcruz525 <jcruz30525@gmail.com>
2025-07-17 23:36:57 -0400

    changes

--- lib/features/shopping/cubit/shopping_cart_cubit.dart
+++ lib/features/shopping/cubit/shopping_cart_cubit.dart
@@ -3,6 +3,19 @@ import 'package:equatable/equatable.dart';
 
 part 'shopping_cart_state.dart';
 
+class CartItem {
+  final String id;
+  final String name;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.name,
+    required this.price,
+  });
+}
+
 class ShoppingCartCubit extends Cubit<ShoppingCartState> {
   ShoppingCartCubit() : super(ShoppingCartInitial());
+ 
 }
--- lib/features/shopping/cubit/shopping_cart_state.dart
+++ lib/features/shopping/cubit/shopping_cart_state.dart
@@ -1,10 +1,12 @@
 part of 'shopping_cart_cubit.dart';
 
-sealed class ShoppingCartState extends Equatable {
-  const ShoppingCartState();
+class ShoppingCartState extends Equatable {
+  final List<CartItem> items;
+
+  const ShoppingCartState({this.items = const []});
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [items];
 }
 
 final class ShoppingCartInitial extends ShoppingCartState {}
